90f31c7ef39630c60b94511bcd4865f0b6ea9037
p0t4t0sandwich <p0t4t0sandwich@gmail.com>
2024-11-06 21:40:32 -0600

    Updated to function with AMP v2.6+

--- WatchFerret.py
+++ WatchFerret.py
@@ -6,14 +6,13 @@ from time import sleep
 import yaml
 import os
 
-from ampapi.modules.ADS import ADS
-from ampapi.modules.CommonAPI import CommonAPI
-
+from ampapi.auth import RefreshingAuthProvider
+from ampapi.modules import ADS, CommonAPI
 
 class WatchFerret():
     def __init__(self, config_path: str):
         self.config_path = config_path
-        self.instance_dict: dict = {}
+        self.instnaces: dict[str, CommonAPI] = {}
 
         # Get host, username, password from config
         with open(self.config_path, 'r') as f:
@@ -21,6 +20,8 @@ class WatchFerret():
             f.close()
 
         self.host = config["global"]["host"]
+        if self.host[-1] != "/":
+            self.host += "/"
         self.username = config["global"]["username"]
         self.password = config["global"]["password"]
 
@@ -66,58 +67,19 @@ class WatchFerret():
             print("Config Error, instance not found in config")
             return {}
 
-    # Instance Type
-    class Instance:
-        def __init__(self, name: str = None, id: str = None, API: CommonAPI = None):
-            self.name = name
-            self.id = id
-            self.API = API
-
     # Instance Login
-    def instanceLogin(self, instance: Instance) -> bool:
-        if instance.name != None:
-            # Get Instance ID
-            if (instance.id == None):
-                # Loop through the targets
-                for target in self.ADS.ADSModule.GetInstances():
-
-                    # Loop through the target instances
-                    for inst in target.AvailableInstances:
-                        instanceModule: str = inst.Module
-
-                        # Check if the instance is a Minecraft instance and grab the instance id
-                        if instanceModule == "Minecraft":
-                            instanceName: str = inst.InstanceName
-                            if instanceName == instance.name:
-                                id: str = inst.InstanceID
-
-                                # Save id back to config file
-                                try:
-                                    with open("./config.yml", 'r') as f:
-                                        config = yaml.safe_load(f)
-                                        f.close()
-
-                                    config["instances"][instanceName]["id"] = id
-
-                                    with open("./config.yml", 'w') as f:
-                                        yaml.dump(config, f, default_flow_style=False)
-                                        f.close()
-                                except:
-                                    print("Error writing instance id to config file")
-
-                                instance.id = id
-                                break
-
-                        # Break if instance id is found
-                        if instance.id != None:
-                            break
-
-            instance.API = self.ADS.InstanceLogin(instance.id, "Minecraft")
-
-            if instance.API != None:
-                self.instance_dict[instance.name] = instance
-                return True
-
+    def instanceLogin(self, instanceName: str, serverName: str) -> bool:
+        if instanceName != None:
+            self.instnaces[serverName] = CommonAPI(
+                RefreshingAuthProvider(
+                    panelUrl=f"{self.host}API/ADSModule/Servers/{instanceName}/",
+                    username=self.username,
+                    password=self.password
+            ))
+            try:
+                return self.instnaces[serverName]._authprovider.Login().success
+            except:
+                return False
         return False
 
     # Monitor
@@ -131,12 +93,13 @@ class WatchFerret():
             instance_conf = self.get_config(serverName)
             try:
                 # Get Server State
-                status = self.instance_dict[serverName].API.Core.GetStatus().State
+                status_enum = self.instnaces[serverName].Core.GetStatus().State
+                status = status_enum.value
                 restart_threshold = instance_conf["restart_threshold"]
                 start_threshold = instance_conf["start_threshold"]
                 stop_threshold = instance_conf["stop_threshold"]
 
-                message = f"Server Status: {status}"
+                message = f"Server Status: {status_enum.name} ({status})"
 
                 if restart_threshold != -1 and status == 30:
                     self.restart_tracker[serverName] += 1
@@ -163,29 +126,28 @@ class WatchFerret():
                 if restart_threshold != -1 and self.restart_tracker[serverName] >= restart_threshold:
                     self.logger(instance_conf["logging_path"], serverName, f"Watchdog Event Detected On Server Restart")
                     self.restart_tracker[serverName] = 0
-                    self.instance_dict[serverName].API.Core.Kill()
+                    self.instnaces[serverName].Core.Kill()
                     sleep(10)
-                    self.instance_dict[serverName].API.Core.Start()
+                    self.instnaces[serverName].Core.Start()
                     self.logger(instance_conf["logging_path"], serverName, f"Attempting to rescue: {serverName}")
                 elif start_threshold != -1 and self.start_tracker[serverName] >= start_threshold:
                     self.logger(instance_conf["logging_path"], serverName, f"Watchdog Event Detected On Server Start")
                     self.start_tracker[serverName] = 0
-                    self.instance_dict[serverName].API.Core.Kill()
+                    self.instnaces[serverName].Core.Kill()
                     sleep(10)
-                    self.instance_dict[serverName].API.Core.Start()
+                    self.instnaces[serverName].Core.Start()
                     self.logger(instance_conf["logging_path"], serverName, f"Attempting to rescue: {serverName}")
                 elif stop_threshold != -1 and self.stop_tracker[serverName] >= stop_threshold:
                     self.logger(instance_conf["logging_path"], serverName, f"Watchdog Event Detected On Server Stop")
                     self.stop_tracker[serverName] = 0
-                    self.instance_dict[serverName].API.Core.Kill()
+                    self.instnaces[serverName].Core.Kill()
                     sleep(10)
-                    self.instance_dict[serverName].API.Core.Start()
+                    self.instnaces[serverName].Core.Start()
                     self.logger(instance_conf["logging_path"], serverName, f"Attempting to rescue: {serverName}")
 
             # On error, try to re-authenticate with AMP
             except Exception as e:
-                instance: self.Instance = self.instance_dict[serverName]
-                result: bool = self.instanceLogin(instance)
+                result: bool = self.instnaces[serverName]._authprovider.Login().success
                 if result == True:
                     self.logger(instance_conf["logging_path"], serverName, f"Re-Authenticating with AMP\n{e}")
                 else:
@@ -195,8 +157,12 @@ class WatchFerret():
 
     # Start
     def start(self) -> None:
-        self.ADS = ADS(self.host, self.username, self.password)
-        self.ADS.Login()
+        auth_provider = RefreshingAuthProvider(
+            panelUrl=self.host,
+            username=self.username,
+            password=self.password
+        )
+        self.ADS = ADS(auth_provider)
 
         # Get instances from config file
         with open(self.config_path, 'r') as f:
@@ -206,15 +172,12 @@ class WatchFerret():
         for serverName in config["instances"].keys():
             # Get instance name and id
             name: str = config["instances"][serverName]["name"]
-            id: str = config["instances"][serverName]["id"] if "id" in config["instances"][serverName].keys() else None
-
-            instance: self.Instance = self.Instance(name, id)
-            status: bool = self.instanceLogin(instance)
+            status: bool = self.instanceLogin(name, serverName)
             if status == True:
-                print(f"Instance {instance.name} is online!")
+                print(f"Instance {name} is online!")
                 Thread(target=self.monitor, args=(serverName,)).start()
             else:
-                print(f"Instance {instance.name} is offline!")
+                print(f"Instance {name} is offline!")
 
 
 if __name__ == "__main__":
